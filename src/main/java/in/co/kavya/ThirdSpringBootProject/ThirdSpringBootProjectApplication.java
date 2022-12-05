0d2d5f0a0a8b313f501677145de39f2f521f86f9
Kavya Mamilla <kavyamamilla@Kavyas-MacBook-Pro.local>
2022-12-05 10:03:24 -0700

    this is my third spring project

---
+++ src/main/java/in/co/kavya/ThirdSpringBootProject/ThirdSpringBootProjectApplication.java
@@ -0,0 +1,13 @@
+package in.co.kavya.ThirdSpringBootProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThirdSpringBootProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThirdSpringBootProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/co/kavya/ThirdSpringBootProject/ThirdSpringBootProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package in.co.kavya.ThirdSpringBootProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThirdSpringBootProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
